2e5f5e060baf96701beb03f3acf1bc61ad2b759d
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2018-03-04 11:04:50 -0600

    Fix note duration quantization

--- quantize.py
+++ quantize.py
@@ -15,9 +15,12 @@ notes = db["notes"]
 def quantize(tick):
     offset = tick % QUANTIZE
     if offset < QUANTIZE/2:
-        return tick-offset
+        new_tick = tick-offset
     else:
-        return tick-offset+QUANTIZE
+        new_tick = tick-offset+QUANTIZE
+    if tick != new_tick:
+        print("Quantizing {} to {}".format(tick, new_tick))
+    return new_tick
 
 def move(src, dst):
     print("{} -> {}".format(src, dst))
@@ -26,7 +29,21 @@ def move(src, dst):
     notes[dst].extend(notes[src])
     del notes[src]
 
-ticks = list(notes.keys())
+ticks = sorted(notes.keys())
+for tick, next_tick in zip(ticks, ticks[1:]):
+    for note in notes[tick]:
+        bar = tick//24//4
+        beat = tick/24%4
+        print("{bar:3}.{beat:3} | {note.note:3} | {note.velocity:3} | {note.duration:3}"
+              .format(bar=bar, beat=beat, note=note))
+        note.duration = quantize(note.duration)
+        if note.duration == 0:
+            note.duration = quantize(next_tick - tick)
+for note in notes[ticks[-1]]:
+    if note.duration == 0:
+        note.duration = 24
+       
+ticks = sorted(notes.keys())
 for src in ticks:
     dst = quantize(src)
     if src != dst:
@@ -37,7 +54,7 @@ first = ticks[0]
 if first > 0:
     for tick in ticks:
         move(tick, tick-first)
-        
+
 db["notes"] = notes
 
 db.close()
