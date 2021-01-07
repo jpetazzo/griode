8372a1b79a9f969912c1075614b1fd570c34a999
Worik <5387413+worikgh@users.noreply.github.com>
2021-01-07 08:11:47 +0000

    Here it is....

--- Songs/README.txt
+++
@@ -1,5 +0,0 @@
-ecasound -f:16,2,48000 \
--i jack_multi,system:capture_1,system:capture_2 \
--o jack_multi,system:playback_1,system:playback_2 \
--el:1795,0.9,0.5,0.3,0.2
-
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,5 +1,8 @@
+#![warn(clippy::all)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::process::Command;
 
+// Endpoints
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -10,9 +13,25 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+// `/hey`
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
+
+async fn list_jack_ports() -> impl Responder {
+    let jack_ports = get_jack_ports().unwrap();
+    HttpResponse::Ok().body(jack_ports)
+}
+
+// Commands to execute
+fn get_jack_ports() -> std::io::Result<Vec<u8>> {
+    let output = Command::new("jack_lsp").arg("-cpt").output().unwrap();
+    Ok(output.stdout)
+}
+fn list_songs()  -> std::io::Result<Vec<u8>> {
+    
+}
+// 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,6 +40,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+	    .route("/jack", web::get().to(list_jack_ports))
     })
     .bind("0.0.0.0:8080")?
     .run()
