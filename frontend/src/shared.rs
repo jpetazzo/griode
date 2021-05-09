84b76ce2b20b33525752e1ab6277f29af86976ca
Worik <5387413+worikgh@users.noreply.github.com>
2021-05-10 08:41:57 +1200

    Add Debug to structures

--- frontend/src/shared.rs
+++ frontend/src/shared.rs
@@ -1,14 +1,14 @@
 use serde::{Deserialize, Serialize};
 
 /// Message from the server to the client.
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ServerMessage {
     pub id: usize,
     pub text: String,
 }
 
 /// Message from the client to the server.
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ClientMessage {
     pub text: String,
 }
